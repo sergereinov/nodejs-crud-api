e42beefa45d5fb96e3f3647c111582996c278014
sergereinov <47950629+sergereinov@users.noreply.github.com>
2023-01-05 01:51:56 +0300

    refactor: make it more readable

--- src/loadBalancer/loadBalancer.ts
+++ src/loadBalancer/loadBalancer.ts
@@ -3,74 +3,99 @@ import { usersBaseUrl } from '../api/baseUrl';
 import { logger } from '../logger/logger';
 import * as Status from '../api/status';
 
+/**
+ * Starts http-server with Load Balancer.
+ */
 export const run = (host: string, listenPort: number, firstWorkerPort: number, workersCount: number) => {
     var workersHost = host;
     var nextPort = firstWorkerPort;
 
-    http
-        .createServer(async (request, response) => {
-            const { pathname } = new URL(request.url, `http://localhost`);
-            if (pathname.startsWith(usersBaseUrl)) {
+    http.createServer(async (request, response) => {
 
-                logger(`Load balancer received a request: ${request.method} ${request.url}`);
+        // Extract base path:
+        //   - split '/api/users/other-things' to ['', 'api', 'users', 'other-things']
+        //   - join it into '/api/users'
+        const basePath = request.url.split('/').slice(0, 3).join('/');
+        if (basePath === usersBaseUrl) {
 
-                const workerPort = nextPort;
+            logger(`Load balancer received a request: ${request.method} ${request.url}`);
 
-                nextPort++;
-                if (nextPort >= firstWorkerPort + workersCount) {
-                    nextPort = firstWorkerPort;
-                }
+            const workerPort = nextPort;
 
-                const opt = {
-                    hostname: workersHost,
-                    port: workerPort,
-                    method: request.method,
-                    path: pathname,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                };
-
-                const workerReq = http.request(opt, async (workerRes) => {
-                    response.statusCode = workerRes.statusCode;
-                    response.statusMessage = workerRes.statusMessage;
-                    response.setHeader('Content-Type', 'application/json');
-
-                    // Body
-                    for await (const chunk of workerRes) {
-                        response.write(chunk);
-                    }
-
-                    //Finish
-                    response.end();
-
-                    logger('Load balancer response:', workerRes.statusCode, workerRes.statusMessage);
-                });
-
-                workerReq.on('error', (e) => {
-                    logger(`Load balancer has problem with worker at ${workerPort} port:`, e);
-                });
-
-                // Body
-                for await (const chunk of request) {
-                    workerReq.write(chunk);
-                }
-
-                // Finish
-                workerReq.end();
-            } else {
-
-                //404
-                const { code, text } = Status.notFound();
-                const msg = 'unknown endpoint for load balancer';
-                [response.statusCode, response.statusMessage] = [code, text];
-                response.setHeader('Content-Type', 'application/json');
-                response.end(JSON.stringify({ error: code, message: msg }));
-
-                logger(`${msg}: ${request.method} ${request.url}`);
+            nextPort++;
+            if (nextPort >= firstWorkerPort + workersCount) {
+                nextPort = firstWorkerPort;
             }
-        })
+
+            // Retranslate request to API-worker
+            retranslateRequest(request, workersHost, workerPort, async (workerResponse) => {
+                // Read reply from API-worker
+                response.statusCode = workerResponse.statusCode;
+                response.statusMessage = workerResponse.statusMessage;
+                response.setHeader('Content-Type', 'application/json');
+
+                // Read body from API-worker
+                for await (const chunk of workerResponse) {
+                    response.write(chunk);
+                }
+
+                // Done
+                response.end();
+
+                logger('Load balancer response:', workerResponse.statusCode, workerResponse.statusMessage);
+            });
+
+        } else {
+
+            // Unknown basePath
+            const { code, text } = Status.notFound();
+            const msg = 'unknown endpoint for load balancer';
+            [response.statusCode, response.statusMessage] = [code, text];
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify({ error: code, message: msg }));
+
+            logger(`${msg}: ${request.method} ${request.url}`);
+
+        }
+    })
         .listen(listenPort, host, () => {
             logger(`Load balancer is running on http://${host}:${listenPort}`);
         });
+}
+
+/**
+ * Retranslate request to API-worker
+ */
+const retranslateRequest = async (
+    request: http.IncomingMessage,
+    workerHost: string,
+    workerPort: number,
+    callback: (res: http.IncomingMessage) => void
+) => {
+
+    // Prepare options
+    const options = {
+        hostname: workerHost,
+        port: workerPort,
+        method: request.method,
+        path: request.url,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Create request to API-worker
+    const workerReq =
+        http.request(options, callback)
+            .on('error', (e) => {
+                logger(`Load balancer has problem with worker at ${workerPort} port:`, e);
+            });
+
+    // Send body to API-worker
+    for await (const chunk of request) {
+        workerReq.write(chunk);
+    }
+
+    // Done
+    workerReq.end();
 }
