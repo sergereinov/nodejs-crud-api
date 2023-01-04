cc2a99c9e6be93f8885f8e45ed5139ea238ebd00
sergereinov <47950629+sergereinov@users.noreply.github.com>
2023-01-04 21:46:10 +0300

    refactor: enhance logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,2 +1,11 @@
-export const logger = (...args: any[]): void =>
+export const baseLogger = (...args: any[]): void =>
     console.log(new Date(), 'pid', process.pid, ...args);
+
+var currentLogger = baseLogger;
+
+export const getLogger = () => currentLogger
+export const setLogger = (cb: (...args: any[]) => void) => {
+    currentLogger = cb;
+}
+
+export const logger = (...args: any[]): void => currentLogger(args);
