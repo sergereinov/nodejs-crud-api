a89fed719a4036472023e7d9b144bff3d9515078
sergereinov <47950629+sergereinov@users.noreply.github.com>
2023-01-12 04:52:45 +0300

    feat: add webpack bundling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import cluster from 'node:cluster';
-//import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv'
 import * as multi from './multi';
 import * as single from './single';
 
-//dotenv.config();
+dotenv.config();
 
 const host = 'localhost';
 const port = +process.env.PORT || 4000;
